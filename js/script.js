c0ce22f834e127ce2ca5d2cf8887176566693eef
kharismatara <kharismataram@gmail.com>
2023-03-19 20:44:42 +0700

    navbar

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+//ketika menu di click
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+const navextra = document.querySelector(".navbar-extra");
+//ketika menu di click
