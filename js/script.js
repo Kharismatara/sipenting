acae299c61c226eaad8fb3a1617f26b5f50a69c1
kharismatara <kharismataram@gmail.com>
2023-03-19 21:16:07 +0700

    menu navbar sudah selesai

--- js/script.js
+++ js/script.js
@@ -4,5 +4,11 @@ const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
-const navextra = document.querySelector(".navbar-extra");
-//ketika menu di click
+
+//click diluar sidebar untuk nav
+const menu = document.querySelector("#menu");
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
